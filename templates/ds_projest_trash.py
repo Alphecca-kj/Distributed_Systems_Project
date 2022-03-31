a26e46bbf160bed8a6c75553a60c94c598db587e
Alphecca-kj <73780550+Alphecca-kj@users.noreply.github.com>
2022-03-31 21:07:05 +0300

    Add files via upload

---
+++ templates/ds_projest_trash.py
@@ -0,0 +1,84 @@
+### Putty ###
+Host_name = user@83.212.80.28
+passwd = MITSOS1 mitsos1
+
+192.168.0.1 vm1
+192.168.0.2 vm2
+192.168.0.3 vm3
+192.168.0.4 vm4
+192.168.0.5 vm5
+
+sudo vim /etc/hostname
+sudo vim /etc/hosts
+sudo vim /etc/bashrc
+sudo vim /etc/apf/conf.apf
+
+sudo apt-get install python3-pip
+
+ssh -p 22 user@snf-28142.ok-kno.grnetcloud.net ----- vm1
+ssh -p 22 user@snf-28143.ok-kno.grnetcloud.net ----- vm2
+ssh -p 22 user@vm2
+ssh -p 22 user@snf-28144.ok-kno.grnetcloud.net ----- vm3
+ssh -p 22 user@vm3
+ssh -p 22 user@snf-28145.ok-kno.grnetcloud.net ----- vm4
+ssh -p 22 user@vm4
+ssh -p 22 user@snf-28146.ok-kno.grnetcloud.net ----- vm5
+ssh -p 22 user@vm5
+
+vm1 LwWXxllt2G
+vm2 LOfmu5EGCL
+vm3 LQDivctx1C
+vm4 gQKzXRjI9l
+vm5 OtMfqjLH4J
+
+chmod +x 
+./
+
+sudo modprobe ip_tables
+sudo vim /etc/modules
+
+grep -i iptables /boot/config-`uname -r`
+sudo modprobe ip6table_filter
+docker run -ti --privileged ubuntu:latest
+### usefull links ###
+# 0 github links 
+# https://github.com/search?q=ntua+distributed
+
+# 1st Ordered Dictionary
+# https://www.geeksforgeeks.org/ordereddict-in-python/
+
+# 2nd SHA - 256 Cryptography hashes
+# https://pycryptodome.readthedocs.io/en/latest/src/hash/sha256.html
+
+# 3rd .__dict__ (creates dictionary)
+# https://stackoverflow.com/questions/19907442/explain-dict-attribute
+
+# 4th SHA
+# https://www.geeksforgeeks.org/sha-in-python/
+# https://pycryptodome.readthedocs.io/en/latest/src/hash/hash.html
+
+# 5th RSA crypto public_key/rsa
+# https://pycryptodome.readthedocs.io/en/latest/src/public_key/rsa.html
+
+
+# 6th dport
+# https://www.digitalocean.com/community/tutorials/iptables-essentials-common-firewall-rules-and-commands
+
+# 7th
+# https://linuxconfig.org/how-to-turn-on-off-ip-forwarding-in-linux
+
+# 8th checking internet connection
+# https://linuxconfig.org/how-to-test-internet-connection-on-linux
+
+# 9th
+# 
+
+# 10th
+#
+
+# 11th
+# 
+
+# 12th
+#
+
